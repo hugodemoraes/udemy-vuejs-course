2982cdcb82d852060f8c48dce7c3f3724a58214d
Hugo de Moraes <hugoc.bmoraes@gmail.com>
2017-03-23 00:08:44 -0300

    Resolving the exercise 6 with my way

---
+++ Exercises/Assignment-6/src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <header>
+                    <h1>Server Status</h1>
+                </header>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <ul class="list-group">
+                    <li
+                            class="list-group-item"
+                            v-for="index in 5">
+                        Server #{{ index }}
+                    </li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <p>Server Details are currently not updated</p>
+            </div>
+        </div>
+        <hr>
+        <app-server-details></app-server-details>
+    </div>
+</template>
+
+<script>
+import ServerDetails from './ServerDetails.vue';
+
+export default {
+    components: {
+        'app-server-details': ServerDetails
+    }
+}
+</script>
+
+<style>
+</style>
